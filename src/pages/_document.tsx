a27ab4ae775ff1f4786428dcc4713e7e12b625ab
Manish <bmanish001@.com>
2024-08-31 15:41:40 +0530

    DSCVR

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+// pages/_document.tsx or src/pages/_document.tsx
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="dscvr:canvas:version" content="vNext" />
+        <meta name="og:image" content="https://your-domain.com/crt.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/carrot-challenge.tsx
@@ -0,0 +1,7 @@
+// src/pages/carrot-challenge.tsx
+
+import { CarrotDeFiChallenge } from '../components/CarrotDeFiChallenge';
+
+export default function CarrotChallengePage() {
+  return <CarrotDeFiChallenge />;
+}
